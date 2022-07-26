4acc594e5b37d4b6d02c21b5f781148231069d2a
Víctor González Prieto <victor.gp.dev@gmail.com>
2022-07-26 16:31:55 +0200

    Don't exit on build error
    Keeps the watcher up. No point in failing un-gracefully and leaving the
    dev with an unresponsive Extension Host.
    
    Also adds a bunch of watcher logs so the user knows what's going on.

--- scripts/watch.js
+++ scripts/watch.js
@@ -1,10 +1,21 @@
 const { watch } = require("fs");
-const build = require('./build');
+const build = require("./build");
+
+const srcName = "src/Lucario.yml"
 
 module.exports = async () => {
-    watch('src/Lucario.yml', async (eventType, filename) => {
+    watch(srcName, async (eventType, filename) => {
         if (eventType == "change") {
-            await build();
+            try {
+                console.log("watch: rebuild...");
+                await build();
+                console.log("watch: ok. waiting for file changes...\n");
+            } catch (error) {
+                delete error.mark.buffer // don't show the entire file
+                console.error(error);
+                console.error("watch: build error. see above.")
+                console.log("watch: waiting for file changes...\n");
+            }
         }
     });
     console.log("watch: on\n");
