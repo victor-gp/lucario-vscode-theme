fb34b4b1745e46d45939f77486e2c3c3f4cfdd4d
Víctor González Prieto <victor.gp.dev@gmail.com>
2023-12-23 19:00:00 +0100

    Add function to inject yaml-color-hex schema
    This is the bit that motivated making this task into an script.
    
    Doing this manually would be a hassle. Search + Replace could work, but
    the keys are not always in the same order.

--- scripts/adapt-schemas.js
+++ scripts/adapt-schemas.js
@@ -7,7 +7,8 @@ module.exports = async () => {
     //todo: automate download from https://github.com/wraith13/vscode-schemas/
 
     const files = await fsp.readdir(SCHEMAS_DIR);
-    const schemaPaths = files.filter((file) => file.endsWith(".json"))
+    const schemaPaths = files
+        .filter(file => file.endsWith(".json") && file !== "yaml-color-hex.json")
         .map(file => path.join(SCHEMAS_DIR, file));
 
     for (const schemaPath of schemaPaths) {
@@ -23,16 +24,34 @@ if (require.main === module) {
     module.exports();
 }
 
+//tsk: find a better name, perhaps adaptIncompatibleBits
 function performReplacements(schema) {
-    // the original files come with $ref uris like "vscode://schemas/workbench-colors"
-    // replace them for relative paths (in the same directory)
-    if (schema['$ref'] && schema['$ref'].startsWith('vscode://schemas/')) {
-        const relativePath = schema['$ref'].replace('vscode://schemas/', '') + '.json';
-        schema['$ref'] = relativePath;
-    }
+    replaceVscodeUris(schema);
+    replaceColorHexTypes(schema);
 
     for (let key in schema) {
         if (typeof schema[key] === 'object')
             performReplacements(schema[key]);
+    }
+}
+
+// the original schemas come with $ref URIs like "vscode://schemas/workbench-colors"
+// replace them for relative paths (no dirname as they're in the same directory)
+function replaceVscodeUris(schema) {
+    if (schema['$ref'] && schema['$ref'].startsWith('vscode://schemas/')) {
+        const relativePath = schema['$ref'].replace('vscode://schemas/', '') + '.json';
+        schema['$ref'] = relativePath;
+    }
+}
+
+// the original schema is doesn't allow some YAML themes' patterns:
+// nullable colors (usually placeholders or commented) and !alpha tags (color + alpha channel)
+// the yaml-color-hex.json schema covers that
+function replaceColorHexTypes(schema) {
+    if (schema['type'] === 'string' && schema['format'] === 'color-hex') {
+        delete schema['type'];
+        delete schema['format'];
+        //nit: place the $ref key first like type, not last
+        schema['$ref'] = 'yaml-color-hex.json'
     }
 }
