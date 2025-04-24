68ea82ab0032f06780a1f434dfb102bc5b7d37f8
Víctor González Prieto <victor.gp.dev@gmail.com>
2025-04-24 17:12:22 +0200

    patch-schemas: Fix the schemas' base url
    I don't know if GitHub changed their urls for raw files or this never
    worked in the first place...
    
    I tested by changing the version from latest to v1.47.3 (earlies) and
    back. The schema files' contents changed, so this works for sure now.

--- scripts/utils/patch-schemas.js
+++ scripts/utils/patch-schemas.js
@@ -7,7 +7,9 @@ const SCHEMAS = [
     'token-styling.json',
     'workbench-colors.json',
 ];
-const BASE_URL = 'https://raw.githubusercontent.com/wraith13/vscode-schemas/master/en/latest/schemas/';
+//todo Change to https://github.com/andymcintosh/vscode-schemas (recently updated). Original is unmaintained.
+//nice-TBD Or maybe keep my own fork? Run the save-vscode-schemas extension myself (headless vscode?) and point to that repo from here.
+const BASE_URL = 'https://raw.githubusercontent.com/wraith13/vscode-schemas/refs/heads/master/en/latest/schemas/';
 const ROOT_DIR = path.join(__dirname, '..', '..');
 //nice-TBD should I move the schemas/ dir to .vscode? It's for supporting intellisense in the IDE...
 const SCHEMAS_DIR = path.join(ROOT_DIR, 'schemas');
